9154cba909ed01fa8b763c9e1473648b889e67e1
Albert Krewinkel <albert@zeitkraut.de>
2024-08-26 11:36:47 +0200

    Pass writer options explicitly

--- tools/psht.lua
+++ tools/psht.lua
@@ -201,7 +201,6 @@ local ANSI = {
   Inline = {},
 }
 local inlines = function (inlns, opts)
-  opts = opts or PANDOC_WRITER_OPTIONS
   local docs, cur = List{}, nil
   for _, inline in ipairs(inlns) do
     cur = ANSI.Inline[inline.t](inline, opts)
@@ -221,7 +220,6 @@ end
 
 local blocks = function (blks, sep, opts)
   sep = sep or blankline
-  opts = opts or PANDOC_WRITER_OPTIONS
   local docs, cur = List{}, nil
   for _, block in ipairs(blks) do
     cur = ANSI.Block[block.t](block, opts)
@@ -241,51 +239,51 @@ end
 
 ANSI.Pandoc = function (doc, opts)
   footnotes = List{}
-  local d = blocks(doc.blocks, blankline)
+  local d = blocks(doc.blocks, blankline, opts)
   local notes = footnotes:map(function (note, i)
       local prefix = opts.extensions:includes 'unicode'
         and tostring(i):gsub('.', unicode_superscript) .. space
         or concat{format("[^%d]:", i), space}
-      return hang(blocks(footnotes[i], blankline), 4, prefix)
+      return hang(blocks(footnotes[i], blankline, opts), 4, prefix)
   end)
   return concat{d, blankline, concat(notes, blankline)}
 end
 
-ANSI.Block.Para = function(el)
-  return inlines(el.content)
+ANSI.Block.Para = function(el, opts)
+  return inlines(el.content, opts)
 end
 
-ANSI.Block.Plain = function(el)
-  return inlines(el.content)
+ANSI.Block.Plain = function(el, opts)
+  return inlines(el.content, opts)
 end
 
-ANSI.Block.BlockQuote = function(el)
-  return prefixed(nest(blocks(el.content, blankline), 1), ">")
+ANSI.Block.BlockQuote = function(el, opts)
+  return prefixed(nest(blocks(el.content, blankline), 1, opts), ">")
 end
 
 ANSI.Block.Header = function(h, opts)
   if h.level < opts.slide_level then
-    current_section = inlines(h.content)
+    current_section = inlines(h.content, opts)
     local content = h.classes:includes 'big'
       and figlet(current_section:render())
       or hcenter(current_section, opts.columns)
     return font({'bold'}, vcenter(content))
   elseif h.level <= 1 then
     return center(
-      font({'bold', 'underline'}, inlines(h.content)),
+      font({'bold', 'underline'}, inlines(h.content, opts)),
       opts.columns
     )
   elseif h.level <= 2 then
     return center(
-      font({'bold'}, inlines(h.content)),
+      font({'bold'}, inlines(h.content, opts)),
       opts.columns
     )
   elseif h.level <= 3 then
-    return font({'bold', 'underline'}, inlines(h.content))
+    return font({'bold', 'underline'}, inlines(h.content, opts))
   elseif h.level <= 4 then
-    return font('faint', inlines(h.content))
+    return font('faint', inlines(h.content, opts))
   else
-    return font('bold', inlines(h.content))
+    return font('bold', inlines(h.content, opts))
   end
 end
 
@@ -294,14 +292,17 @@ ANSI.Block.Div = function(div, opts)
     -- omit notes
     return empty
   elseif div.classes:includes 'center' or div.classes:includes 'centered' then
-    local content = blocks(div.content)
+    local content = blocks(div.content, nil, opts)
     local width = div.attributes.width or nil
     return {cr, hcenter(content, width) , blankline}
   elseif div.classes:includes 'section'
      and div.content[1].level == opts.slide_level then
-    return concat{slide_header_row(), cr, blocks(div.content), blankline}
+    return concat{
+      slide_header_row(), cr,
+      blocks(div.content, blankline, opts), blankline
+    }
   else
-    return {cr, blocks(div.content, blankline), blankline}
+    return {cr, blocks(div.content, blankline, opts), blankline}
   end
 end
 
@@ -316,22 +317,22 @@ ANSI.Block.Null = function()
   return empty
 end
 
-ANSI.Block.LineBlock = function(el)
-  return concat(el.content:map(inlines), cr)
+ANSI.Block.LineBlock = function(el, opts)
+  return concat(el.content:map(function (x) return inlines(x, opts) end), cr)
 end
 
 ANSI.Block.Table = function()
   return 'table omitted'
 end
 
-ANSI.Block.DefinitionList = function(el)
+ANSI.Block.DefinitionList = function(el, opts)
   local function render_def (def)
-    return concat{blankline, blocks(def), blankline}
+    return concat{blankline, blocks(def, nil, opts), blankline}
   end
   local function render_item(item)
     local term, defs = unpack(item)
     local inner = concat(defs:map(render_def))
-    return hang(inner, 2, concat{ inlines{pandoc.Strong(term)}, cr })
+    return hang(inner, 2, concat{ inlines({pandoc.Strong(term, opts)}, opts), cr })
   end
   return concat(el.content:map(render_item), blankline)
 end
@@ -340,13 +341,13 @@ ANSI.Block.BulletList = function(ul, opts)
   local bullet = opts.extensions:includes 'unicode' and '• ' or '- '
   bullet = font('green', bullet)
   local function render_item (item)
-    return hang(blocks(item, blankline), 2, bullet):nest(2)
+    return hang(blocks(item, blankline, opts), 2, bullet):nest(2)
   end
   local sep = is_tight_list(ul) and cr or blankline
   return cr .. concat(ul.content:map(render_item), sep)
 end
 
-ANSI.Block.OrderedList = function(ol)
+ANSI.Block.OrderedList = function(ol, opts)
   local result = List{cr}
   local num = ol.start
   local maxnum = num + #ol.content
@@ -359,14 +360,14 @@ ANSI.Block.OrderedList = function(ol)
     (ol.delimiter == pandoc.TwoParens and "(%s)") or
     "%s."
   local num_formatter = format_number[ol.style]
-  for i, item in ipairs(ol.content) do
+  for _, item in ipairs(ol.content) do
     local barenum = num_formatter(num)
     local numstr = format(delimfmt, barenum)
     local sps = width - #numstr
     local numsp = (sps < 1) and space or string.rep(" ", sps)
     result:insert(
       hang(
-        blocks(ol.content[i], blankline),
+        blocks(item, blankline, opts),
         width,
         font('red', concat{numstr,numsp})
       ):nest(2)
@@ -416,21 +417,21 @@ end
 ANSI.Inline.Emph = function(em, opts)
   local color = opts.extensions:includes 'color' and 'green' or nil
   return opts.extensions:includes 'italic'
-    and font({'italic', color}, inlines(em.content))
-    or font({'underline', color}, inlines(em.content))
+    and font({'italic', color}, inlines(em.content, opts))
+    or font({'underline', color}, inlines(em.content, opts))
 end
 
 ANSI.Inline.Strong = function(strong, opts)
   local color = opts.extensions:includes 'color' and 'red' or nil
-  return font({'bold', color}, inlines(strong.content))
+  return font({'bold', color}, inlines(strong.content, opts))
 end
 
-ANSI.Inline.Strikeout = function(el)
-  return font('strikeout', inlines(el.content))
+ANSI.Inline.Strikeout = function(el, opts)
+  return font('strikeout', inlines(el.content, opts))
 end
 
-ANSI.Inline.Subscript = function(el)
-  return { '~', inlines(el.content), '~'}
+ANSI.Inline.Subscript = function(el, opts)
+  return { '~', inlines(el.content, opts), '~'}
 end
 
 ANSI.Inline.Superscript = function(el, opts)
@@ -444,25 +445,25 @@ ANSI.Inline.Superscript = function(el, opts)
   end
   local result = el:walk {Str = tosuperscript}
   if opts.extensions:includes 'unicode' and all_unicode then
-    return inlines(result.content)
+    return inlines(result.content, opts)
   else
-    return { '^', inlines(el.content), '^'}
+    return { '^', inlines(el.content, opts), '^'}
   end
 end
 
-ANSI.Inline.SmallCaps = function(el)
+ANSI.Inline.SmallCaps = function(el, opts)
   local function to_upper (str)
     return pandoc.text.upper(str.text)
   end
-  return inlines(el.content:walk {Str = to_upper})
+  return inlines(el.content:walk {Str = to_upper}, opts)
 end
 
-ANSI.Inline.Underline = function(u)
-  return font('underline', inlines(u.content))
+ANSI.Inline.Underline = function(u, opts)
+  return font('underline', inlines(u.content, opts))
 end
 
-ANSI.Inline.Cite = function(el)
-  return inlines(el.content)
+ANSI.Inline.Cite = function(el, opts)
+  return inlines(el.content, opts)
 end
 
 ANSI.Inline.Math = function(el)
@@ -470,33 +471,36 @@ ANSI.Inline.Math = function(el)
   return { marker, ANSI.Inline.Code(el) }
 end
 
-ANSI.Inline.Span = function(span)
+ANSI.Inline.Span = function(span, opts)
   if span.classes:includes 'mark' then
-    return font({'magentabg'}, inlines(span.content))
+    return font({'magentabg'}, inlines(span.content, opts))
   end
-  return inlines(span.content)
+  return inlines(span.content, opts)
 end
 
-ANSI.Inline.Link = function(link)
+ANSI.Inline.Link = function(link, opts)
   if link.target:match '^%#' then
     -- drop internal links
-    return inlines(link.content)
+    return inlines(link.content, opts)
   elseif link.target == stringify(link.content) then
     -- drop autolinks
-    return inlines(link.content)
+    return inlines(link.content, opts)
   else
-    return inlines(link.content .. {pandoc.Note(pandoc.Plain{link.target})})
+    return inlines(
+      link.content .. {pandoc.Note(pandoc.Plain{link.target})},
+      opts
+    )
   end
 end
 
-ANSI.Inline.Image = function(el)
-  return inlines(el.caption)
+ANSI.Inline.Image = function(el, opts)
+  return inlines(el.caption, opts)
 end
 
-ANSI.Inline.Quoted = function(q)
+ANSI.Inline.Quoted = function(q, opts)
   return q.quotetype == pandoc.DoubleQuote
-    and inlines(q.content):double_quotes()
-    or  inlines(q.content):quotes()
+    and inlines(q.content, opts):double_quotes()
+    or  inlines(q.content, opts):quotes()
 end
 
 ANSI.Inline.Note = function(note, opts)
@@ -527,7 +531,6 @@ local write_title_slide = function (meta)
 end
 
 Writer = function (doc, opts)
-  PANDOC_WRITER_OPTIONS = opts
   opts.slide_level = opts.slide_level or pandoc.structure.slide_level(doc)
   doc.blocks = pandoc.structure.make_sections(doc, opts)
   local slides_directory = '_slides'
